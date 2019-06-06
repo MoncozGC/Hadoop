2a041ebee7d3f377a3eabf68ae3b2ea1b6ab9176
JadeQi <v.jadepen@gmail.com>
2019-06-06 22:59:37 +0800

    第七步: 自定义reduce逻辑

---
+++ hadoop_01/hadoop_hdfs_mr/src/main/java/com/JadePenG/mr/WordCountReducer.java
@@ -0,0 +1,64 @@
+package com.JadePenG.mr;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * 第七步: 自定义reduce逻辑
+ */
+public class WordCountReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+    private LongWritable conutScore = new LongWritable();
+
+    /**
+     * 每接收一行数据就执行reduce方法
+     * @param key
+     * @param values
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+
+        /*
+         * 数据格式
+         *
+         * K2:hello
+         * V2:{1,1}
+         *
+         * 处理为
+         * K3:hello
+         * V3:1+1=2
+         */
+        //1. 接收数据
+        //2. 处理数据
+        long count = 0;
+        for (LongWritable value : values) {
+            count +=value.get();
+
+        }
+        conutScore.set(count);
+        //3.发送到下游
+
+        context.write(key, conutScore);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
