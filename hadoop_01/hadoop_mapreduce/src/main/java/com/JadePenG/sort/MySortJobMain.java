7993ddcaa9a81800ecf170c57854e2cdca712bd5
JadeQi <v.jadepen@gmail.com>
2019-06-07 12:53:17 +0800

    排序主方法

---
+++ hadoop_01/hadoop_mapreduce/src/main/java/com/JadePenG/sort/MySortJobMain.java
@@ -0,0 +1,55 @@
+package com.JadePenG.sort;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class MySortJobMain extends Configured implements Tool {
+
+
+    @Override
+    public int run(String[] strings) throws Exception {
+
+        Job job = Job.getInstance(super.getConf(), "mySort");
+        job.setJarByClass(MySortJobMain.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job,
+                new Path("H:\\~Big Data\\Employment\\03_大数据阶段\\day04_MapReduce增强\\资料\\排序\\input"));
+
+        //2. 自定义mapper逻辑
+        job.setMapperClass(MySortMapper.class);
+        job.setOutputKeyClass(SortBean.class);
+        job.setOutputValueClass(Text.class);
+
+        //7. 自定义reduce逻辑
+        job.setReducerClass(MySortReducer.class);
+        job.setOutputKeyClass(SortBean.class);
+        job.setOutputValueClass(Text.class);
+
+        //8. 指定输出
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job,
+                new Path("H:\\~Big Data\\Employment\\03_大数据阶段\\day04_MapReduce增强\\资料\\排序\\output"));
+
+        //等待完成
+        boolean b = job.waitForCompletion(true);
+
+        System.out.println(Job.JobState.DEFINE);
+
+        return b ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int run = ToolRunner.run(new Configuration(), new MySortJobMain(), args);
+        System.exit(run);
+
+    }
+}
