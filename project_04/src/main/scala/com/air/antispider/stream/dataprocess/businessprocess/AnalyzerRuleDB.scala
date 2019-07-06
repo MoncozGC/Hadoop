4307fe7ccfa47a826cf3d1811e929439381b2996
JadeQi <v.jadepen@gmail.com>
2019-07-06 17:36:35 +0800

    添加queryRule方法  读取数据库中的请求解析规则

--- project_04/src/main/scala/com/air/antispider/stream/dataprocess/businessprocess/AnalyzerRuleDB.scala
+++ project_04/src/main/scala/com/air/antispider/stream/dataprocess/businessprocess/AnalyzerRuleDB.scala
@@ -1,6 +1,9 @@
 package com.air.antispider.stream.dataprocess.businessprocess
 
-import com.air.antispider.stream.common.util.database.QueryDB
+import java.sql.{Connection, PreparedStatement, ResultSet}
+
+import com.air.antispider.stream.common.bean.AnalyzeRule
+import com.air.antispider.stream.common.util.database.{QueryDB, c3p0Util}
 import com.air.antispider.stream.dataprocess.constants.{BehaviorTypeEnum, FlightTypeEnum}
 
 import scala.collection.mutable
@@ -10,6 +13,7 @@ import scala.collection.mutable.ArrayBuffer
   * 数据库的操作类
   */
 object AnalyzerRuleDB {
+
 
   /**
     * 查询过滤规则
@@ -31,18 +35,18 @@ object AnalyzerRuleDB {
   /**
     * 查询分类规则信息
     */
-  def queryClassifyRule() = {
+  def queryClassifyRule(): mutable.HashMap[String, ArrayBuffer[String]] = {
     //国内查询
-    val nqSql = "select expression from nh_classify_rule where flight_type="+FlightTypeEnum.National.id+" and operation_type="+BehaviorTypeEnum.Query.id
+    val nqSql = "select expression from nh_classify_rule where flight_type=" + FlightTypeEnum.National.id + " and operation_type=" + BehaviorTypeEnum.Query.id
 
     //国际查询
-    val iqSql = "select expression from nh_classify_rule where flight_type="+FlightTypeEnum.International.id+" and operation_type="+BehaviorTypeEnum.Query.id
+    val iqSql = "select expression from nh_classify_rule where flight_type=" + FlightTypeEnum.International.id + " and operation_type=" + BehaviorTypeEnum.Query.id
 
     //国内预定
-    val nbSql = "select expression from nh_classify_rule where flight_type="+FlightTypeEnum.National.id+" and operation_type="+BehaviorTypeEnum.Book.id
+    val nbSql = "select expression from nh_classify_rule where flight_type=" + FlightTypeEnum.National.id + " and operation_type=" + BehaviorTypeEnum.Book.id
 
     //国际预定
-    val ibSql = "select expression from nh_classify_rule where flight_type="+FlightTypeEnum.International.id+" and operation_type="+BehaviorTypeEnum.Book.id
+    val ibSql = "select expression from nh_classify_rule where flight_type=" + FlightTypeEnum.International.id + " and operation_type=" + BehaviorTypeEnum.Book.id
 
     //指定查询的字段
     val field = "expression"
@@ -54,11 +58,75 @@ object AnalyzerRuleDB {
     val ibList = QueryDB.queryData(ibSql, field)
 
     //创建Map， 将规则数据进行封装到map对象  这样只需要广播一次
-    val ruleMap = new mutable.HashMap[String, ArrayBuffer[String]]()
+    val ruleMap: mutable.HashMap[String, ArrayBuffer[String]] = new mutable.HashMap[String, ArrayBuffer[String]]()
     ruleMap.put("NationalQueryList", nqList)
     ruleMap.put("InternationalQueryList", iqList)
     ruleMap.put("NationalBookList", nbList)
     ruleMap.put("InternationalBookList", ibList)
+    //返回HashMap
     ruleMap
   }
+
+  /**
+    * 读取数据库中的请求解析规则
+    */
+  def queryRule(behaviorType: Int) = {
+    val analyzeRuleList = new ArrayBuffer[AnalyzeRule]()
+    //定义查询的sql
+    val sql = "select * from analyzerule where behavior_type=" + behaviorType
+
+    var conn: Connection = null
+    var ps: PreparedStatement = null
+    var rs: ResultSet = null
+    try {
+      conn = c3p0Util.getConnection
+      ps = conn.prepareStatement(sql)
+      rs = ps.executeQuery()
+      while (rs.next()) {
+        val analyzeRule = new AnalyzeRule()
+        analyzeRule.id = rs.getString("id")
+        analyzeRule.flightType = rs.getString("flight_type").toInt
+        analyzeRule.BehaviorType = rs.getString("behavior_type").toInt
+        analyzeRule.requestMatchExpression = rs.getString("requestMatchExpression")
+        analyzeRule.requestMethod = rs.getString("requestMethod")
+        analyzeRule.isNormalGet = rs.getString("isNormalGet").toBoolean
+        analyzeRule.isNormalForm = rs.getString("isNormalForm").toBoolean
+        analyzeRule.isApplicationJson = rs.getString("isApplicationJson").toBoolean
+        analyzeRule.isTextXml = rs.getString("isTextXml").toBoolean
+        analyzeRule.isJson = rs.getString("isJson").toBoolean
+        analyzeRule.isXML = rs.getString("isXML").toBoolean
+        analyzeRule.formDataField = rs.getString("formDataField")
+        analyzeRule.book_bookUserId = rs.getString("book_bookUserId")
+        analyzeRule.book_bookUnUserId = rs.getString("book_bookUnUserId")
+        analyzeRule.book_psgName = rs.getString("book_psgName")
+        analyzeRule.book_psgType = rs.getString("book_psgType")
+        analyzeRule.book_idType = rs.getString("book_idType")
+        analyzeRule.book_idCard = rs.getString("book_idCard")
+        analyzeRule.book_contractName = rs.getString("book_contractName")
+        analyzeRule.book_contractPhone = rs.getString("book_contractPhone")
+        analyzeRule.book_depCity = rs.getString("book_depCity")
+        analyzeRule.book_arrCity = rs.getString("book_arrCity")
+        analyzeRule.book_flightDate = rs.getString("book_flightDate")
+        analyzeRule.book_cabin = rs.getString("book_cabin")
+        analyzeRule.book_flightNo = rs.getString("book_flightNo")
+        analyzeRule.query_depCity = rs.getString("query_depCity")
+        analyzeRule.query_arrCity = rs.getString("query_arrCity")
+        analyzeRule.query_flightDate = rs.getString("query_flightDate")
+        analyzeRule.query_adultNum = rs.getString("query_adultNum")
+        analyzeRule.query_childNum = rs.getString("query_childNum")
+        analyzeRule.query_infantNum = rs.getString("query_infantNum")
+        analyzeRule.query_country = rs.getString("query_country")
+        analyzeRule.query_travelType = rs.getString("query_travelType")
+        analyzeRule.book_psgFirName = rs.getString("book_psgFirName")
+        analyzeRuleList += analyzeRule
+      }
+    } catch {
+      case e: Exception => e.printStackTrace()
+    } finally {
+      c3p0Util.close(conn, ps, rs)
+    }
+    analyzeRuleList.toList
+  }
+
+
 }
