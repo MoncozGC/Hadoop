19a6862a2b15d6ba60a8d4ca7e02eee16c8a4b9a
JadeQi <v.jadepen@gmail.com>
2019-07-07 21:48:50 +0800

    TODO 11. 数据推送 TODO 12. 监控数据流量 添加了setupSsc方法参数的描述

--- project_04/src/main/scala/com/air/antispider/stream/dataprocess/launch/DataProcessLaunch.scala
+++ project_04/src/main/scala/com/air/antispider/stream/dataprocess/launch/DataProcessLaunch.scala
@@ -60,9 +60,9 @@ object DataProcessLaunch {
     * 26/Jun/2019:03:42:54 -0800#CS#POST /B2C40/query/jaxb/direct/query.ao HTTP/1.1#CS#POST#CS#application/x-www-form-urlencoded; charset=UTF-8#CS#json=%7B%22depcity%22%3A%22CAN%22%2C+%22arrcity%22%3A%22WUH%22%2C+%22flightdate%22%3A%2220180220%22%2C+%22adultnum%22%3A%221%22%2C+%22childnum%22%3A%220%22%2C+%22infantnum%22%3A%220%22%2C+%22cabinorder%22%3A%220%22%2C+%22airline%22%3A%221%22%2C+%22flytype%22%3A%220%22%2C+%22international%22%3A%220%22%2C+%22action%22%3A%220%22%2C+%22segtype%22%3A%221%22%2C+%22cache%22%3A%220%22%2C+%22preUrl%22%3A%22%22%2C+%22isMember%22%3A%22%22%7D#CS#http://b2c.csair.com/B2C40/modules/bookingnew/main/flightSelectDirect.html?t=S&c1=CAN&c2=WUH&d1=2019-07-02&at=1&ct=0&it=0#CS#192.168.180.1#CS#Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36#CS#2019-06-26T03:42:54-08:00#CS#192.168.180.111#CS#JSESSIONID=782121159357B98CA6112554CF44321E; sid=b5cc11e02e154ac5b0f3609332f86803; aid=8ae8768760927e280160bb348bef3e12; identifyStatus=N; userType4logCookie=M; userId4logCookie=13818791413; useridCookie=13818791413; userCodeCookie=13818791413; temp_zh=cou%3D0%3Bsegt%3D%E5%8D%95%E7%A8%8B%3Btime%3D2018-01-13%3B%E5%B9%BF%E5%B7%9E-%E5%8C%97%E4%BA%AC%3B1%2C0%2C0%3B%26cou%3D1%3Bsegt%3D%E5%8D%95%E7%A8%8B%3Btime%3D2019-07-02%3B%E5%B9%BF%E5%B7%9E-%E6%88%90%E9%83%BD%3B1%2C0%2C0%3B%26; JSESSIONID=782121159357B98CA6112554CF44321E; WT-FPC=id=211.103.142.26-608782688.30635197:lv=1516170718655:ss=1516170709449:fs=1513243317440:pn=2:vn=10; language=zh_CN; WT.al_flight=WT.al_hctype(S)%3AWT.al_adultnum(1)%3AWT.al_childnum(0)%3AWT.al_infantnum(0)%3AWT.al_orgcity1(CAN)%3AWT.al_dstcity1(CTU)%3AWT.al_orgdate1(2019-07-02)
     * 26/Jun/2019:03:42:54 -0800#CS#POST /B2C40/dist/main/images/common.png HTTP/1.1#CS#POST#CS#application/x-www-form-urlencoded; charset=UTF-8#CS#json=%7B%22depcity%22%3A%22CAN%22%2C+%22arrcity%22%3A%22WUH%22%2C+%22flightdate%22%3A%2220180220%22%2C+%22adultnum%22%3A%221%22%2C+%22childnum%22%3A%220%22%2C+%22infantnum%22%3A%220%22%2C+%22cabinorder%22%3A%220%22%2C+%22airline%22%3A%221%22%2C+%22flytype%22%3A%220%22%2C+%22international%22%3A%220%22%2C+%22action%22%3A%220%22%2C+%22segtype%22%3A%221%22%2C+%22cache%22%3A%220%22%2C+%22preUrl%22%3A%22%22%2C+%22isMember%22%3A%22%22%7D#CS#http://b2c.csair.com/B2C40/modules/bookingnew/main/flightSelectDirect.html?t=S&c1=CAN&c2=WUH&d1=2018-02-20&at=1&ct=0&it=0#CS#192.168.180.1#CS#Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36#CS#2019-06-26T03:42:54-08:00#CS#192.168.180.111#CS#JSESSIONID=782121159357B98CA6112554CF44321E; sid=b5cc11e02e154ac5b0f3609332f86803; aid=8ae8768760927e280160bb348bef3e12; identifyStatus=N; userType4logCookie=M; userId4logCookie=13818791413; useridCookie=13818791413; userCodeCookie=13818791413; temp_zh=cou%3D0%3Bsegt%3D%E5%8D%95%E7%A8%8B%3Btime%3D2018-01-13%3B%E5%B9%BF%E5%B7%9E-%E5%8C%97%E4%BA%AC%3B1%2C0%2C0%3B%26cou%3D1%3Bsegt%3D%E5%8D%95%E7%A8%8B%3Btime%3D2018-01-17%3B%E5%B9%BF%E5%B7%9E-%E6%88%90%E9%83%BD%3B1%2C0%2C0%3B%26; JSESSIONID=782121159357B98CA6112554CF44321E; WT-FPC=id=211.103.142.26-608782688.30635197:lv=1516170718655:ss=1516170709449:fs=1513243317440:pn=2:vn=10; language=zh_CN; WT.al_flight=WT.al_hctype(S)%3AWT.al_adultnum(1)%3AWT.al_childnum(0)%3AWT.al_infantnum(0)%3AWT.al_orgcity1(CAN)%3AWT.al_dstcity1(CTU)%3AWT.al_orgdate1(2018-01-17)
     *
-    * @param sc
-    * @param kafkaParams
-    * @param topic
+    * @param sc          sparkContext对象
+    * @param kafkaParams kafka的集群地址
+    * @param topic       指定消费的topic
     */
   def setupSsc(sc: SparkContext, kafkaParams: Map[String, String], topic: Set[String]) = {
     //创建streamingContext对象
@@ -152,7 +152,7 @@ object DataProcessLaunch {
       val accessLogRDD: RDD[AccessLog] = DataSplit.parseAccessLog(rdd)
 
       //TODO 3. 链路统计 要写入redis中所以传一个redis
-      BusinessProcess.linkCount(accessLogRDD, jedis)
+      val serversCountMap: collection.Map[String, Int] = BusinessProcess.linkCount(accessLogRDD, jedis)
 
       //TODO 4. 数据清洗（将符合过滤规则的数据清洗掉）
       val filterRDD: RDD[AccessLog] = accessLogRDD.filter(accessLog => UrlFilter.filterUrl(accessLog, filterRuleRef.value))
@@ -195,18 +195,25 @@ object DataProcessLaunch {
 
         //TODO 10. 数据格式化
         val processedData: ProcessedData = DataPackage.dataPackage("", record, highFreIp, requestTypeLabel, travelTypeLabel, queryRequestData, bookRequestData)
-        
 
-        processedData
         //测试解析完的查询数据
         //bookRequestData
         //测试数据格式化数据
-        //processedData
+        processedData
       })
       //测试解析完的查询数据
       //processedRDD.foreach(println(_))
       //测试数据格式化数据 toKafkaString推送到kafka的数据结构化数据
       processedRDD.foreach(record => println(record.toKafkaString()))
+
+      //TODO 11. 数据推送, 将拼接好的字符串发送到kafka中
+      //将查询的数据发送到查询的topic中
+      DataSend.sendQueryDataToKafka(processedRDD)
+      //将预定的数据发送到预定的topic中
+      DataSend.sendBookDataToKafka(processedRDD)
+
+      //TODO 12. 监控数据流量(每处理一批次数据就请求一次监控地址获取当前批次计算的结束时间和开始时间得到计算时长)
+      SparkStreamingMonitor.streamingMonitor(sc, rdd, serversCountMap, jedis)
 
       //TODO 释放资源
       rdd.unpersist()
