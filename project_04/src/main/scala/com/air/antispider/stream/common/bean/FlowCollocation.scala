c1568881a49a3bce8fe3b469fc0719c7b4114269
JadeQi <v.jadepen@gmail.com>
2019-07-09 20:26:44 +0800

    添加描述

--- project_04/src/main/scala/com/air/antispider/stream/common/bean/FlowCollocation.scala
+++ project_04/src/main/scala/com/air/antispider/stream/common/bean/FlowCollocation.scala
@@ -1,12 +1,18 @@
 package com.air.antispider.stream.common.bean
 
 /**
- * 流程类：对应表nh_process_info
- * （规则配置集合和阈值参数）
- */
+  * 流程类：对应表nh_process_info
+  * （规则配置集合和阈值参数）
+  *
+  * @param flowId         流程ID
+  * @param flowName       流程名字
+  * @param rules          流程的八项规则
+  * @param flowLimitScore 阈值(当用户打分超过这个值就是爬虫ip)
+  * @param strategyCode   策略Code(直接使用流程ID代替)
+  */
 case class FlowCollocation(
                             flowId: String,
                             flowName: String,
                             rules: List[RuleCollocation],
                             flowLimitScore: Double = 100,
-                            strategyCode:String)
+                            strategyCode: String)
